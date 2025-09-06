b7b86c9fc4341fb87e7b69390d21c56bf39970a2
gilett4 <vadymudod@hotmail.com>
2025-09-06 10:44:47 +0300

    Settings file update.

---
+++ Runtime/Tools/MenuSettings.cs
@@ -0,0 +1,8 @@
+namespace HootyBird.Minimal.Tools
+{
+    public static class MenuSettings
+    {
+        public const string MainMenuControllerName = "Main Menu Controller";
+        public const string GameplayMenuControllerName = "Gameplay Menu Controller";
+    }
+}
